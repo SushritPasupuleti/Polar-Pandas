4caaa6feed7ff0781edc349f199adf822917a21e
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2024-04-12 09:09:04 +0530

    feat: add pandasql

--- app.py
+++ app.py
@@ -3,18 +3,21 @@ from pandasql import sqldf
 
 # load the dataset & set backend to pyarrow
 df = pd.read_csv("data/insurance-w-claims.csv").convert_dtypes(dtype_backend="pyarrow")
+df["id"] = df.index
 
 print(df.dtypes)
 
 print("Source: \n", df.head())
 
-sql = "SELECT * FROM df WHERE age > 50 ORDER BY age ASC"
+sql = "SELECT *, rank() OVER (PARTITION BY claim ORDER BY claim DESC, id) AS overall_claim_rank, rank() OVER (PARTITION BY job_title ORDER BY claim DESC, id) AS job_wise_claim_rank  FROM df WHERE age > 50 ORDER BY age ASC"
 
 res = sqldf(sql, locals())
 
 print("SQL Res: \n", res.head())
 
+# res = res.sort_values(by="claim_rank", ascending=True)
+
 sorted = df[df["age"] > 50]
 sorted = sorted.sort_values(by="age", ascending=True)
 
-print("Sorted: \n", sorted.head())
+# print("Sorted: \n", sorted.head())
